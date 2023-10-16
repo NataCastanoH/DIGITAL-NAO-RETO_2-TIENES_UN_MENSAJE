0c2b8abde27aebf1e20e8471d504da3eb03bd20d
unknown <nataliahoyos@gmail.com>
2023-10-16 09:32:45 -0500

    carga inicial

--- mensaje_semanal.sh
+++ mensaje_semanal.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Este es tu script para enviar el correo semanal
-# Inserta aquí tu código para enviar el correo
+
+# Ubicación del script de Python (mensaje_semanal.py)
+script_python = "C:\Users\Natalia\Recursos DN_COM_58\DIGITAL-NAO-RETO_2-TIENES_UN_MENSAJE" 
+
+# Comprueba si hoy es lunes (día de la semana 1 en cron)
+if [ "$(date +\%u)" = "1" ]; then
+    # Ejecuta el script de Python para enviar el correo a las 8:00 a. m.
+    python3 $script_python
+fi
 
 chmod +x mensaje_semanal.sh
-
-crontab -e
-
